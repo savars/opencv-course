b5698cf1438bb20d5fb0d08f9ff900224028f2bd
savars <75224427+savars@users.noreply.github.com>
2021-05-05 00:18:52 +0200

    Update contours.py

--- Section #1 - Basics/contours.py
+++ Section #1 - Basics/contours.py
@@ -15,16 +15,21 @@ cv.imshow('Gray', gray)
 blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
 
-canny = cv.Canny(blur, 125, 175)
+#edges
+#contours decreases canny edges if you apply it in blured pic
+canny = cv.Canny(blur, 125, 175) 
 cv.imshow('Canny Edges', canny)
 
-# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY) # below 125 is set as 0 in treshold function.
 # cv.imshow('Thresh', thresh)
 
+# takes edges
 contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contour(s) found!')
 
-cv.drawContours(blank, contours, -1, (0,0,255), 1)
-cv.imshow('Contours Drawn', blank)
+cv.drawContours(blank, contours, -1, (0,0,255), 1) # number of images you want can be chosen here:1
+cv.imshow('Contours Drawn', blank) 
+
+#trasholding alone can cause misleading while converting contours. 
 
 cv.waitKey(0)
