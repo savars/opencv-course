afd729ee569fe7ccd992ae67c748835b1ddd1d37
savars <75224427+savars@users.noreply.github.com>
2021-05-05 22:49:16 +0200

    Update colour_spaces.py

--- Section #2 - Advanced/colour_spaces.py
+++ Section #2 - Advanced/colour_spaces.py
@@ -1,4 +1,6 @@
 #pylint:disable=no-member
+#TUTORIAL ABOUT CONVERTING BETWEEN COLOR SPACES!
+#If you need to convert from Grayscale to L*a*b, first Grayscale ---> BGR then BGR ----> L*a*b
 
 import cv2 as cv
 import matplotlib.pyplot as plt
@@ -7,25 +9,31 @@ img = cv.imread('../Resources/Photos/park.jpg')
 cv.imshow('Park', img)
 
 # plt.imshow(img)
-# plt.show()
+# plt.show() 
 
+
+# Converting images from BGR to .... ;
 # BGR to Grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 # BGR to HSV
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-cv.imshow('HSV', hsv)
+cv.imshow('HSV', hsv) 
 
-# BGR to L*a*b
+# BGR to L*a*b 
 lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
-cv.imshow('LAB', lab)
+cv.imshow('LAB', lab) 
 
 # BGR to RGB
 rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 cv.imshow('RGB', rgb)
 
 # HSV to BGR
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow('HSV --> BGR', hsv_bgr)
+
+#LAB to BGR
 lab_bgr = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
 cv.imshow('LAB --> BGR', lab_bgr)
 
